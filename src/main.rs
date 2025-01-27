dfa77c5e0dd17c5524c91fb992b4b637e31cec27
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-01-27 13:02:55 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,23 @@ rustup --version                       # check rustup and rustc version
 rustc main.rs                          # compile a file
 rustfmt main.rs                        # format source a file
 cargo new training_project             # create new project with cargo tool
-cargo build                            # build an application in debug mode
 cargo run                              # build and run an application in debug mode
+cargo build                            # build an application in debug mode
 cargo build --release                  # build an application in release mode
+cargo clean                            # clean project
 cargo check                            # check/build code without generating executables
 cargo fmt                              # format source files in the project
-cargo clean                            # clean project
+cargo clippy                           # check project with linter
 */
 
+mod exercises;
 mod language_basics;
+mod memory_management;
 
 fn main() {
     // language_basics::run();
+    // fibonacci::run();
+    // money::run();
+    // money_with_enums::run();
+    memory_management::run();
 }
